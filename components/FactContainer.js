7794e0ff8b2b52446aa3d92af937653237afd1c9
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-02 23:35:30 +0000

    Worked more on getStaticProps for daily fact

--- components/FactContainer.js
+++ components/FactContainer.js
@@ -2,38 +2,9 @@ import React, { useEffect, useState } from 'react'
 const xml2js = require('xml2js')
 const { DOMParser } = require('@xmldom/xmldom');
 
-
-// async function getDailyEvent() {
-// 	var parser = new DOMParser();
-//   var factArray = [];
-
-// 	fetch("https://api.hiztory.org/random/event.xml", {
-// 		"method": "GET",
-// 		"headers": {
-// 		}
-// 	})
-// 	.then(response => response.text())
-// 	.then(str => parser.parseFromString(str, "text/xml"))
-// 	.then(data => xml2js.parseString(data, (err, result) => {
-// 		if(err) {
-// 			throw err;
-// 		}
-
-//     //Push content and answer
-// 		factArray.push(JSON.stringify(result['event']['event'][0]['$']['content'], null, 4))
-// 		factArray.push(JSON.stringify(result['event']['event'][0]['$']['date'], null, 4)) 
-// 		// dailyFact = json['content'];
-// 		// dailyAnswer = json['date'].split('-')[0];
-
-//     return factArray;
-// 	}))
-// 	.catch(err => {
-// 		console.error(err);
-// 	});
-// }
-
 function FactContainer(props) {
   
+  // console.log(factArray);
   var myHeaders = new Headers();
   myHeaders.append("Access-Control-Allow-Origin", "*"); 
   
@@ -44,34 +15,19 @@ function FactContainer(props) {
 
   const [fact, setFact] = useState()
   const [answer, setAnswer] = useState()
-  
+
   useEffect(() => {
-    
-    fetch("https://api.hiztory.org/random/event.xml", {
-      		"method": "GET",
-      		"headers": {
-          }
-      	})
-      .then(res => res.text())
-      .then(str => parser.parseFromString(str, 'text/xml'))
-      .then(data => xml2js.parseString(data, (err, result) => {
-        if(err) {
-          throw err;
-        }
-
-        console.log(result['event']['event'][0]['$']['content'])
-
-        setFact(JSON.stringify(result['event']['event'][0]['$']['content'], null, 4));
-        setAnswer(JSON.stringify[result['event']['event'][0]['$']['date'], null, 4]);
-      }))
-  }, []);
-
+    setFact(props.fact) 
+  }, [props.fact])
+  // setFact(props.fact)
   return (
     <div className="grid place-items-center py-12">
         <h3 className="text-white font-semibold text-3xl pb-8">Which year?</h3>
-        <p className="text-white font-medium text-2xl">{ fact ? fact : "Loading..." }</p>
+        <p className="text-white font-medium text-2xl">{ props.fact ? props.fact : "Loading..." }</p>
     </div>
   )
 }
+
+
 
 export default FactContainer
--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,65 @@ import styles from '../styles/Home.module.css'
 import Container from '../components/Container.js'
 import Header from '../components/Header.js'
 import FactContainer from '../components/FactContainer.js'
-
+const xml2js = require('xml2js')
+const { DOMParser } = require('@xmldom/xmldom');
 
 let dailyFact = "";
 let dailyAnswer = "";
 
-
-
-export default function Home() {
+function Home(props) {
 	return (
 		<Container>
 			<Header />
-			<FactContainer fact={dailyFact}/>
+			<FactContainer fact={props.factArray[0]}/>
 		</Container>
 	)
 }
+
+async function getDailyEvent() {
+	var parser = new DOMParser();
+  	var factArray = [];
+
+	fetch("https://api.hiztory.org/random/event.xml", {
+		"method": "GET",
+		"headers": {
+		}
+	})
+	.then(response => response.text())
+	.then(str => parser.parseFromString(str, "text/xml"))
+	.then(data => xml2js.parseString(data, async (err, result) => {
+		if(err) {
+			throw err;
+		}
+    //Push content and answer
+		await factArray.push(JSON.stringify(result['event']['event'][0]['$']['content'], null, 4))
+		await factArray.push(JSON.stringify(result['event']['event'][0]['$']['date'], null, 4)) 
+		
+		// dailyFact = json['content'];
+		// dailyAnswer = json['date'].split('-')[0];
+    	await console.log(factArray)
+	
+	return await factArray;
+	}))
+	.catch(err => {
+		console.error(err);
+	});
+}
+
+
+export async function getStaticProps() {
+  
+	let factArray = []
+
+	factArray = await getDailyEvent();
+
+  	return {
+    	props: {
+    	  factArray: factArray || "Loading...",
+    	},
+		revalidate: 1,
+  	}
+      
+}
+
+export default Home
