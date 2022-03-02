1ab34c220182fa0f5eeda94e97db11998bba8559
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-02 22:43:09 +0000

    Started asynchronous fact loading

--- components/FactContainer.js
+++ components/FactContainer.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+const xml2js = require('xml2js')
+const { DOMParser } = require('@xmldom/xmldom');
+
+
+// async function getDailyEvent() {
+// 	var parser = new DOMParser();
+//   var factArray = [];
+
+// 	fetch("https://api.hiztory.org/random/event.xml", {
+// 		"method": "GET",
+// 		"headers": {
+// 		}
+// 	})
+// 	.then(response => response.text())
+// 	.then(str => parser.parseFromString(str, "text/xml"))
+// 	.then(data => xml2js.parseString(data, (err, result) => {
+// 		if(err) {
+// 			throw err;
+// 		}
+
+//     //Push content and answer
+// 		factArray.push(JSON.stringify(result['event']['event'][0]['$']['content'], null, 4))
+// 		factArray.push(JSON.stringify(result['event']['event'][0]['$']['date'], null, 4)) 
+// 		// dailyFact = json['content'];
+// 		// dailyAnswer = json['date'].split('-')[0];
+
+//     return factArray;
+// 	}))
+// 	.catch(err => {
+// 		console.error(err);
+// 	});
+// }
 
 function FactContainer(props) {
-    console.log("TEST: " + props.fact);
+  
+  var myHeaders = new Headers();
+  myHeaders.append("Access-Control-Allow-Origin", "*"); 
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+  };
+
+  const [fact, setFact] = useState()
+  const [answer, setAnswer] = useState()
+  
+  useEffect(() => {
+    
+    fetch("https://api.hiztory.org/random/event.xml", {
+      		"method": "GET",
+      		"headers": {
+          }
+      	})
+      .then(res => res.text())
+      .then(str => parser.parseFromString(str, 'text/xml'))
+      .then(data => xml2js.parseString(data, (err, result) => {
+        if(err) {
+          throw err;
+        }
+
+        console.log(result['event']['event'][0]['$']['content'])
+
+        setFact(JSON.stringify(result['event']['event'][0]['$']['content'], null, 4));
+        setAnswer(JSON.stringify[result['event']['event'][0]['$']['date'], null, 4]);
+      }))
+  }, []);
+
   return (
     <div className="grid place-items-center py-12">
         <h3 className="text-white font-semibold text-3xl pb-8">Which year?</h3>
-        <p className="text-white font-medium text-2xl">{ props.fact }</p>
+        <p className="text-white font-medium text-2xl">{ fact ? fact : "Loading..." }</p>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -14,43 +14,13 @@ import Container from '../components/Container.js'
 import Header from '../components/Header.js'
 import FactContainer from '../components/FactContainer.js'
 
-const { DOMParser } = require('@xmldom/xmldom');
-const xml2js = require('xml2js')
 
 let dailyFact = "";
 let dailyAnswer = "";
 
-async function getDailyEvent() {
-	var parser = new DOMParser();
 
-	fetch("https://api.hiztory.org/random/event.xml", {
-		"method": "GET",
-		"headers": {
-		}
-	})
-	.then(response => response.text())
-	.then(str => parser.parseFromString(str, "text/xml"))
-	.then(data => xml2js.parseString(data, (err, result) => {
-		if(err) {
-			throw err;
-		}
-
-		dailyFact = JSON.stringify(result['event']['event'][0]['$']['content'], null, 4);
-		dailyAnswer = JSON.stringify(result['event']['event'][0]['$']['date'], null, 4);
-		// dailyFact = json['content'];
-		// dailyAnswer = json['date'].split('-')[0];
-
-		console.log(dailyAnswer + " : " + dailyFact);
-	}))
-	.catch(err => {
-		console.error(err);
-	});
-}
 
 export default function Home() {
-
-	await getDailyEvent();
-
 	return (
 		<Container>
 			<Header />
